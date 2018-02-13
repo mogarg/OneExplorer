bb7b77b075fee8d19da05c115ef44cc0a7f9d7b8
Mohit Garg <mohitce@vt.edu>
2018-02-12 19:07:37 -0500

    Initial Commit

---
+++ OneExplorer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneExplorer
+//
+//  Created by Mohit Garg on 2/12/18.
+//  Copyright © 2018 Mohit Garg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
