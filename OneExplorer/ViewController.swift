b8e994d11f0d781c6cc37f406c6d0ed2a66829f9
Mohit Garg <mohitce@vt.edu>
2018-02-14 18:21:36 -0500

    Init

--- OneExplorer/ViewController.swift
+++ OneExplorer/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var fetch: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +21,9 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func fetchBlock(_ sender: Any) {
+        
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,21 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'OneExplorer' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for OneExplorer
+
+  target 'OneExplorerTests' do
+    inherit! :search_paths
+    # Pods for testing
+    pod 'SwiftyJSON', '~> 2.3'
+  end
+
+  target 'OneExplorerUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
